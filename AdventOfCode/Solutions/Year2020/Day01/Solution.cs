bec3007db7f88aa47bba9c7f1bbca66e28c2bfba
Gustav Persson <gustav@ustwo.com>
2020-12-01 13:49:17 +0100

    Day 1

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -8,18 +9,46 @@ namespace AdventOfCode.Solutions.Year2020
     class Day01 : ASolution
     {
 
+        int[] Expenses;
+
         public Day01() : base(01, 2020, "")
         {
-
+            Expenses = Input.ToIntArray("\n").Where(e => e < 2020).ToArray();
         }
 
         protected override string SolvePartOne()
         {
+            foreach (var expense1 in Expenses)
+            {
+                foreach (var expense2 in Expenses)
+                {
+                    if (expense1 + expense2 == 2020)
+                    {
+                        return (expense1 * expense2).ToString();
+                    }
+                }
+
+            }
             return null;
         }
 
         protected override string SolvePartTwo()
         {
+            foreach (var expense1 in Expenses)
+            {
+                foreach (var expense2 in Expenses)
+                {
+                    foreach (var expense3 in Expenses)
+                    {
+
+                        if (expense1 + expense2 + expense3 == 2020)
+                        {
+                            return (expense1 * expense2 * expense3).ToString();
+                        }
+                    }
+                }
+
+            }
             return null;
         }
     }
