4a84923284b0db4eccf915d3ec7eb9ac4378f74e
Gustav Persson <gustav@ustwo.com>
2020-12-06 10:05:49 +0100

    Day 6: Custom Customs

--- AdventOfCode/Solutions/Year2020/Day06/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day06/Solution.cs
@@ -1,26 +1,53 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
 
     class Day06 : ASolution
     {
+        string[] Declarations;
 
-        public Day06() : base(06, 2020, "")
+        public Day06() : base(06, 2020, "Custom Customs")
         {
-
+            Declarations = Input.Split(new string[] { Environment.NewLine + Environment.NewLine },
+                                           StringSplitOptions.RemoveEmptyEntries);
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            int totalCounts = 0;
+            foreach (var declaration in Declarations)
+            {
+                string cleanedString = declaration.Replace("\n", "").Replace("\r", "");
+                int uniqueAnswers = cleanedString.Distinct().ToArray().Length;
+                totalCounts += uniqueAnswers;
+            }
+            return totalCounts.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            int totalCounts = 0;
+            foreach (var declaration in Declarations)
+            {
+                string[] people = declaration.Split(new string[] { Environment.NewLine },
+                               StringSplitOptions.RemoveEmptyEntries);
+
+                int commonAnswers = 0;
+                foreach (var character in people[0].ToCharArray())
+                {
+                    int count = declaration.Count(f => f == character);
+                    if (count == people.Length)
+                    {
+                        commonAnswers += 1;
+                    }
+                }
+                totalCounts += commonAnswers;
+            }
+            return totalCounts.ToString();
         }
     }
 }
