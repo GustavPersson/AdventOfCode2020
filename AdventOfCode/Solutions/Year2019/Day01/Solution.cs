badabade993ca80256f38a21da3dfa1a20411943
Gustav Persson <gustav@ustwo.com>
2020-12-10 14:01:36 +0100

    Day 1: The Tyranny of the Rocket Equation

--- AdventOfCode/Solutions/Year2019/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2019/Day01/Solution.cs
@@ -1,26 +1,59 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2019
 {
 
     class Day01 : ASolution
     {
+        int[] Modules;
+        int FuelMass;
 
-        public Day01() : base(01, 2019, "")
+        public Day01() : base(01, 2019, "The Tyranny of the Rocket Equation")
         {
-
+            Modules = Input.ToIntArray("\n");
         }
+
+        private static int CalculateFuelNeeded(int mass)
+        {
+            int fuelNeeded = (int)Math.Floor(mass / 3.0) - 2;
+
+            return fuelNeeded;
+        }
+
 
         protected override string SolvePartOne()
         {
-            return null;
+            int fuelNeeded = Modules.Aggregate(0, (sum, next) => sum + CalculateFuelNeeded(next));
+            FuelMass = fuelNeeded;
+            return fuelNeeded.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            int fuelNeeded = Modules.Aggregate(0, (sum, next) =>
+            {
+                int fuelNeeded = CalculateFuelNeeded(next);
+                int fuelNeedingFuel = fuelNeeded;
+                int totalFuelNeeded = fuelNeeded;
+                while (fuelNeedingFuel > 0)
+                {
+
+                    int fuelNeededForFuel = CalculateFuelNeeded(fuelNeedingFuel);
+
+                    if (fuelNeededForFuel <= 0)
+                    {
+                        break;
+                    }
+                    fuelNeedingFuel = fuelNeededForFuel;
+                    totalFuelNeeded += fuelNeededForFuel;
+                }
+                return sum + totalFuelNeeded;
+            });
+
+            return fuelNeeded.ToString();
         }
     }
 }
