81327c597aaef119c8e094a4ef92bdcb0b40b5b5
Gustav Persson <gustav@ustwo.com>
2020-12-15 14:08:31 +0100

    Day 15: Rambunctious Recitation

--- AdventOfCode/Solutions/Year2020/Day15/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day15/Solution.cs
@@ -1,26 +1,70 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
 
     class Day15 : ASolution
     {
-
-        public Day15() : base(15, 2020, "")
+        List<int> StartingNumbers;
+        List<int> SpokenNumbers;
+        Dictionary<int, int> LastTimeSpoken;
+        public Day15() : base(15, 2020, "Rambunctious Recitation")
         {
+            StartingNumbers = Input.ToIntArray(",").ToList();
+        }
+
+        private string PlayMemoryGame(int iterations)
+        {
+
+            SpokenNumbers.Capacity = iterations;
+
+            int PreviousNumberSpoken = 0;
+
+            for (int currentRound = StartingNumbers.Count; currentRound < iterations; currentRound++)
+            {
+                PreviousNumberSpoken = SpokenNumbers[^1];
+                var lastTimeSpoken = LastTimeSpoken.GetValueOrDefault(PreviousNumberSpoken, -1);
+                if (lastTimeSpoken == -1)
+                {
+                    lastTimeSpoken = 0;
+                }
+                else
+                {
+                    lastTimeSpoken = currentRound - lastTimeSpoken;
+                }
+                LastTimeSpoken[PreviousNumberSpoken] = currentRound;
+                SpokenNumbers.Add(lastTimeSpoken);
+            }
+
+            return SpokenNumbers[^1].ToString();
 
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            LastTimeSpoken = new Dictionary<int, int>();
+            SpokenNumbers = new List<int>(StartingNumbers);
+
+            for (int i = 1; i <= StartingNumbers.Count; i++)
+            {
+                LastTimeSpoken.Add(StartingNumbers[i - 1], i);
+            }
+            return PlayMemoryGame(2020);
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            LastTimeSpoken = new Dictionary<int, int>();
+            SpokenNumbers = new List<int>(StartingNumbers);
+
+            for (int i = 1; i <= StartingNumbers.Count; i++)
+            {
+                LastTimeSpoken.Add(StartingNumbers[i - 1], i);
+            }
+            return PlayMemoryGame(30000000);
         }
     }
 }
