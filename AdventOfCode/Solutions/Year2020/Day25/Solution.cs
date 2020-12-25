0d115bc1f255572f2d54d5f9e0553ed9277b6e79
Gustav Persson <gustav@ustwo.com>
2020-12-25 23:48:08 +0100

    Day 25: Combo Breaker

--- AdventOfCode/Solutions/Year2020/Day25/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day25/Solution.cs
@@ -7,20 +7,54 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day25 : ASolution
     {
+        long DoorKey;
+        long CardKey;
 
-        public Day25() : base(25, 2020, "")
+        long DoorLoopSize;
+        long CardLoopSize;
+
+        readonly long SubjectNumber = 7;
+
+        public Day25() : base(25, 2020, "Combo Breaker")
         {
-
+            CardKey = long.Parse(Input.SplitByNewline()[0]);
+            DoorKey = long.Parse(Input.SplitByNewline()[1]);
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            long key = 1;
+            DoorLoopSize = 0;
+            CardLoopSize = 0;
+
+            do
+            {
+                key *= SubjectNumber;
+                key %= 20201227;
+                DoorLoopSize++;
+            } while (key != DoorKey);
+
+            key = 1;
+            do
+            {
+                key *= SubjectNumber;
+                key %= 20201227;
+                CardLoopSize++;
+            } while (key != CardKey);
+
+            key = 1;
+
+            for (long i = 0; i < CardLoopSize; i++)
+            {
+                key *= DoorKey;
+                key %= 20201227;
+            }
+            return key.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            return "God Jul!";
         }
     }
 }
