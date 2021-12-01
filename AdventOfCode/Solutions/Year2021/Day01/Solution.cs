d17e8fa4fda97afe3f0ce4a21a9057e774c228a1
Gustav Persson <gustav@ustwo.com>
2021-12-01 09:13:30 +0100

    Day 01: Sonar Sweep

--- AdventOfCode/Solutions/Year2021/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2021/Day01/Solution.cs
@@ -1,26 +1,60 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2021
 {
 
-    class Day01 : ASolution
+  class Day01 : ASolution
+  {
+    int[] Measurements;
+    int Increases;
+
+    public Day01() : base(01, 2021, "Sonar Sweep")
     {
-
-        public Day01() : base(01, 2021, "")
-        {
-
-        }
-
-        protected override string SolvePartOne()
-        {
-            return null;
-        }
-
-        protected override string SolvePartTwo()
-        {
-            return null;
-        }
+      Measurements = Input.ToIntArray("\n").ToArray();
+      Increases = 0;
     }
+
+    protected override string SolvePartOne()
+    {
+      Increases = 0;
+      int PreviousItem = 0;
+      bool firstRun = true;
+      foreach (var item in Measurements)
+      {
+        if (!firstRun && item > PreviousItem)
+        {
+          Increases++;
+        }
+        PreviousItem = item;
+        firstRun = false;
+      }
+      return Increases.ToString();
+    }
+
+    protected override string SolvePartTwo()
+    {
+      Increases = 0;
+      int PreviousSpan = 0;
+      bool firstRun = true;
+
+      for (int i = 0; i < Measurements.Length; i++)
+      {
+        if ((Measurements.Length - 1) >= i + 2)
+        {
+          int CurrentSpan = Measurements[i] + Measurements[i + 1] + Measurements[i + 2];
+          if (!firstRun && CurrentSpan > PreviousSpan)
+          {
+            Increases++;
+          }
+          PreviousSpan = CurrentSpan;
+        }
+        firstRun = false;
+      }
+
+      return Increases.ToString();
+    }
+  }
 }
