351120c8cb10cad52ac4827c3638a5b5d9dbfa9e
Gustav Persson <gustav.persson@jointacademy.com>
2022-11-30 10:18:43 +0100

    Prep for 2022

---
+++ aoc2022/src/template/index.ts
@@ -0,0 +1,38 @@
+import run from "aocrunner";
+import _ from "lodash";
+import log from "loglevel";
+
+import { Solution } from "../types";
+import { getLines } from "../utils/index.js";
+
+log.enableAll();
+
+const parseInput = (rawInput: string) => getLines(rawInput);
+
+const part1: Solution = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+const part2: Solution = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+run({
+  part1: {
+    tests: [
+      // { input: ``, expected: "" },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // { input: ``, expected: "" },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+});
---
+++ aoc2022/src/utils/index.ts
@@ -0,0 +1,59 @@
+/**
+ * Root for your util libraries.
+ *
+ * You can import them in the src/template/index.ts,
+ * or in the specific file.
+ *
+ * Note that this repo uses ES Modules, so you have to explicitly specify
+ * .js extension (yes, .js not .ts - even for TypeScript files)
+ * for imports that are not imported from node_modules.
+ *
+ * For example:
+ *
+ *   correct:
+ *
+ *     import _ from 'lodash'
+ *     import myLib from '../utils/myLib.js'
+ *     import { myUtil } from '../utils/index.js'
+ *
+ *   incorrect:
+ *
+ *     import _ from 'lodash'
+ *     import myLib from '../utils/myLib.ts'
+ *     import { myUtil } from '../utils/index.ts'
+ *
+ *   also incorrect:
+ *
+ *     import _ from 'lodash'
+ *     import myLib from '../utils/myLib'
+ *     import { myUtil } from '../utils'
+ *
+ */
+ export const TWO_PI = Math.PI * 2;
+
+ export function getLines(rawInput: string): string[] {
+   return rawInput
+     .split(/\n/)
+     .map((l) => l.trim())
+     .filter((l) => l);
+ }
+ 
+ export function getLineGroups(rawInput: string): string[][] {
+   const parts: string[][] = [[]];
+ 
+   for (const line of rawInput.split(/\n/)) {
+     if (!line.trim().length) {
+       parts.push([]);
+     } else {
+       parts[parts.length - 1].push(line);
+     }
+   }
+ 
+   return parts.filter((part) => part.length > 0);
+ }
+ 
+ export function wrapRotation(angle: number): number {
+   while (angle < 0) angle += TWO_PI;
+   return angle % TWO_PI;
+ }
+ 
