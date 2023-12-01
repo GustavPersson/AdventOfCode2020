8b16eabe20a7ab03f7daae05a525433f2a14ba31
Gustav Persson <gustav.persson@consafelogistics.com>
2023-12-02 00:22:59 +0100

    --- Day 1: Trebuchet?! ---

---
+++ aoc2023/src/day01/index.ts
@@ -0,0 +1,75 @@
+import run from "aocrunner";
+import _ from "lodash";
+import log from "loglevel";
+
+import { getLines } from "../utils/index.js";
+
+log.enableAll();
+
+const parseInput = (rawInput: string) => getLines(rawInput);
+
+const digits = { one: "1", two: "2", three: "3", four: "4", five: "5", six: "6", seven: "7", eight: "8", nine: "9", } as const;
+type MatchedString = keyof typeof digits;
+type MatchedNumber = typeof digits[MatchedString];
+
+const regexWords = Object.keys(digits).join("|")
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  const result = input.map((line) => {
+    const numericLine = line.replace(/\D/g, '');
+    const number = numericLine[0] + numericLine[numericLine.length - 1];
+
+    return parseInt(number);
+  }).reduce((acc, curr) => acc + curr, 0);
+
+  return result;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  const result = input.map((line) => {
+    const first = line.match(/\d|one|two|three|four|five|six|seven|eight|nine/)?.[0];
+    const last = line.match(/.*(\d|one|two|three|four|five|six|seven|eight|nine)/)?.[1];
+
+    return _.parseInt((digits[first as MatchedString] ?? first) + (digits[last as MatchedString] ?? last));
+  }).reduce((acc, curr) => acc + curr, 0);
+
+  return result
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet`,
+        expected: 142,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+        `,
+        expected: 281,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
---
+++ aoc2023/src/utils/index.ts
@@ -0,0 +1,65 @@
+/**
+ * Root for your util libraries.
+ *
+ * You can import them in the src/template/index.ts,
+ * or in the specific file.
+ *
+ * Note that this repo uses ES Modules, so you have to explicitly specify
+ * .js extension (yes, .js not .ts - even for TypeScript files)
+ * for imports that are not imported from node_modules.
+ *
+ * For example:
+ *
+ *   correct:
+ *
+ *     import _ from 'lodash'
+ *     import myLib from '../utils/myLib.js'
+ *     import { myUtil } from '../utils/index.js'
+ *
+ *   incorrect:
+ *
+ *     import _ from 'lodash'
+ *     import myLib from '../utils/myLib.ts'
+ *     import { myUtil } from '../utils/index.ts'
+ *
+ *   also incorrect:
+ *
+ *     import _ from 'lodash'
+ *     import myLib from '../utils/myLib'
+ *     import { myUtil } from '../utils'
+ *
+ */
+export function getLines(rawInput: string): string[] {
+    return rawInput
+      .split(/\n/)
+      .map((l) => l.trim())
+      .filter((l) => l);
+  }
+  
+  export function getLineGroups(rawInput: string): string[][] {
+    const parts: string[][] = [[]];
+  
+    for (const line of rawInput.split(/\n/)) {
+      if (!line.trim().length) {
+        parts.push([]);
+      } else {
+        parts[parts.length - 1].push(line);
+      }
+    }
+  
+    return parts.filter((part) => part.length > 0);
+  }
+ 
+  export function getLineGroupsAsNumbers(rawInput: string): number[][] {
+    const parts: number[][] = [[]];
+  
+    for (const line of rawInput.split(/\n/)) {
+      if (!line.trim().length) {
+        parts.push([]);
+      } else {
+        parts[parts.length - 1].push(parseInt(line, 10));
+      }
+    }
+  
+    return parts.filter((part) => part.length > 0);
+  }
